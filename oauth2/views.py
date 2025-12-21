358526679146e7bf406fa73da88c6bece2a10f5c
gundambox <7722420+gundambox@users.noreply.github.com>
2025-12-21 20:26:53 +0800

    issue #14 Implemnt optional api endpoint

--- oauth2/views.py
+++ oauth2/views.py
@@ -4,3 +4,10 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def health_check(request):
     return Response({"status": "ok"})
+
+@api_view(['GET'])
+def version(request):
+    return Response({
+        "service": "MyAuthService",
+        "version": "0.1.0"
+    })
